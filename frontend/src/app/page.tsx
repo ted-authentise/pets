989724c2a0ea0537447c4d82476f7692ee32b7c7
Ted <ted_bennett@icloud.com>
2023-05-22 23:47:32 +0100

    feat: add dog pic fetch

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,19 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
+
 export default function Home() {
+  const { data: dogImageUrl } = useQuery(["dog image"], {
+    queryFn: async () => {
+      const res = await (
+        await fetch("https://dog.ceo/api/breeds/image/random")
+      ).json();
+      const url = (res as { message: string }).message;
+      return url;
+    },
+  });
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <img src={dogImageUrl} width={100} height={100}></img>
+    </main>
   );
 }
