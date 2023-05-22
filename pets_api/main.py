eaafb814c4cdd2f2b8d9456fa4e7f70e1f1beda4
Ted <ted_bennett@icloud.com>
2023-05-22 20:09:36 +0100

    chore: add endpoints, table creation

--- pets_api/main.py
+++ pets_api/main.py
@@ -1,6 +1,32 @@
 from fastapi import FastAPI  
+import sqlite3
+from pydantic import BaseModel
 app = FastAPI()   
     
-@app.get("/") 
-async def main_route():     
-  return {"message": "Hey, It is me Goku"}
+connection = sqlite3.connect("pets.db")
+cursor = connection.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS pets
+               (name text, breed text, type text, ranking real, image text)''')
+
+@app.get("/pets") 
+async def get_by_type(type: str):     
+  return {"search": type}
+
+class Pet(BaseModel):
+    name: str
+    breed: str
+    type: str
+    image: str
+    ranking: int
+
+@app.put("/pets")
+async def add_pet(pet: Pet):
+  return pet
+
+@app.delete("/pets")
+async def delete_by_type(type: str):
+  return {"type": type}
+
+@app.delete("/pets/{name}")
+async def delete_by_name(name):
+  return {"name": name}
